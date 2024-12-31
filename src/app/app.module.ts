12acf978f6d4c5b009860a93612f1c9506be360c
Zain Asif <zain.asif@iomechs.com>
2024-12-31 17:32:36 +0500

    Comment out unused imports and module declarations in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
-import { AppComponent } from './app.component';
-import { TodosModule } from './MyComponenets/todos/todos.module';
+// import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { CommonModule } from '@angular/common';
+// import { AppComponent } from './app.component';
+// import { TodosModule } from './MyComponenets/todos/todos.module';
 
-@NgModule({
-  declarations: [],
-  imports: [BrowserModule, CommonModule, TodosModule],
-  providers: [],
-  bootstrap: [],
-})
-export class AppModule { }
+// @NgModule({
+//   declarations: [],
+//   imports: [BrowserModule, CommonModule, TodosModule],
+//   providers: [],
+//   bootstrap: [],
+// })
+// export class AppModule { }
